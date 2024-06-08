bf936c39f3f46bddacf061709a87d069fe24ff28
UrielSch3 <urielscharf12@gmail.com>
2024-06-07 22:21:14 -0300

    Más avances

--- inicio.py
+++ inicio.py
@@ -1,1 +1,10 @@
+import pandas as pd
+
 print("Este es el inicio del TP")
+
+
+df= pd.read_csv('')
+df= pd.read_csv('')
+df= pd.read_csv('')
+df= pd.read_csv('')
+df= pd.read_csv('')
--- parte1.py
+++ parte1.py
@@ -1,3 +1,4 @@
 import pandas as pd
 
 df = pd.read_csv('C:\Users\Uriel\Desktop\TP1-Parte-3') 
+df2 = pd.read_csv('C:\Users\Uriel\Desktop\TP1-Parte-3') 
